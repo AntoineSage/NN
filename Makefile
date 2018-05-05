a65c608c374953f66b0d46fb23d5ae088d85c373
Antoine <antoinesaget19@gmail.com>
2018-05-06 01:47:04 +0200

    SDL !

--- Makefile
+++ Makefile
@@ -1,15 +1,17 @@
 CC = clang++
-CFLAGS = -Wall -std=gnu++11
+CFLAGS = -Wall -std=gnu++11 
+EXEC_FLAG = -lmingw32 -lSDL2main -lSDL2 
 EXEC_NAME = NN_test.exe
 OBJ_FILES = NN_test.o NN.o Matrix.o
 
 all : $(EXEC_NAME)
 
 debug : CFLAGS += -g
+debug : EXEC_FLAG += -g
 debug : all
 
 $(EXEC_NAME) : $(OBJ_FILES)
-	$(CC) -o $(EXEC_NAME) $(OBJ_FILES) $(LIBS)
+	$(CC) -o $(EXEC_NAME) $(OBJ_FILES) $(EXEC_FLAG)
 
 %.o: %.cpp
 	$(CC) $(CFLAGS) -o $@ -c $<
--- NN_test.cpp
+++ NN_test.cpp
@@ -1,4 +1,5 @@
 #include "NN.h"
+#include <SDL2/SDL.h>
 #include <initializer_list>
 #include <iostream>
 #include <stdio.h>
@@ -6,7 +7,7 @@
 #include <time.h>
 #include <typeinfo>
 #include <vector>
-
+// -LC:/msys64/mingw64/include/SDL2 -lmingw32 -lSDLmain -lSDL
 // "C:/msys64/mingw64/lib/*",
 // "C:/msys64/mingw64/include",
 // "C:/msys64/mingw64/include/c++/7.3.0",
@@ -14,13 +15,28 @@
 // "C:/msys64/mingw64/include/c++/7.3.0/x86_64-w64-mingw32",
 // "C:/msys64/mingw64/x86_64-w64-mingw32/include"
 
-int main() {
+int main(int argc, char **argv) {
   srand(time(NULL));
 
-  NN test(1, 1, 1);
-  test.afficher();
-  std::vector< double > _Input(1.0);
-  test.feedForward(_Input);
-  test.afficher();
+  // NN test(1, 1, 1);
+  // test.afficher();
+  // std::vector< double > _Input(1.0);
+  // test.feedForward(_Input);
+  // test.afficher();
+  
+  SDL_Init(SDL_INIT_EVERYTHING);
+  SDL_Window *window =
+      SDL_CreateWindow("TEST", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
+                       600, 400, SDL_WINDOW_SHOWN);
+  SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, 0);
+
+  SDL_SetRenderDrawColor(renderer, 0, 255, 0, 255);
+
+  SDL_RenderClear(renderer);
+
+  SDL_RenderPresent(renderer);
+
+  SDL_Delay(3000);
+
   return 0;
 }
